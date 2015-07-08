189e4edbcf83213a204377b928400715cee11d67
Philip Linde <plinde@proceranetworks.com>
2015-07-08 19:58:18 +0200

    raw input mode for single key presses

--- televj.go
+++ televj.go
@@ -2,43 +2,27 @@ package main
 
 import (
 	"bufio"
+	"golang.org/x/crypto/ssh/terminal"
 	"log"
 	"os"
 )
 
 func main() {
-	/*
-		anims := map[string]anim{
-			"a": anim{
-				frame{0, 500, "a_f1"},
-				frame{1, 500, "a_f2"},
-				frame{2, 500, "a_f3"},
-				frame{3, 100, "a_f4"},
-				frame{4, 500, "a_f5"},
-				frame{5, 500, "a_f6"},
-			},
-			"b": anim{
-				frame{0, 5000, "b_f1"},
-				frame{1, 5000, "b_f2"},
-				frame{2, 5000, "b_f3"},
-				frame{3, 5000, "b_f4"},
-				frame{4, 1000, "b_f5"},
-				frame{5, 5000, "b_f6"},
-			},
-		}
-	*/
+	oldterm, err := terminal.MakeRaw(0)
+	fatal(err)
+	defer terminal.Restore(0, oldterm)
+
 	anims := loadfiles(os.Args[1])
 	inch := run(anims)
-	reader := bufio.NewReader(os.Stdin)
+	r := bufio.NewReader(os.Stdin)
 
+	log.Println("Running")
 	for {
-		key, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		if key == "quit\n" {
+		key, _, err := r.ReadRune()
+		fatal(err)
+		if key == '\x1b' {
 			os.Exit(0)
 		}
-		inch <- key[:len(key)-1]
+		inch <- string(key)
 	}
 }
